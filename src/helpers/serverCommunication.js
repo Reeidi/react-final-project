1263deb4913f321d9a92778cbe2c6d7519288d14
reeidi <reeidi@abv.bg>
2021-12-12 04:30:45 +0200

    Add server fetching, userService

---
+++ src/helpers/serverCommunication.js
@@ -0,0 +1,61 @@
+import * as userService from "../services/userService.js";
+
+async function request(url, options) {
+    console.log(options);
+    try {
+        const response = await fetch(url, options);
+        console.log('done');
+        if (response.ok === false) {
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        try {
+            return await response.json();
+        } catch (err) {
+            return response;
+        }
+    } catch (err) {
+        alert(err.messege);
+        throw err;
+    }
+}
+
+function getOptions(method, body, needsAuth = false) {
+    const options = {
+        method,
+        headers: {}
+    };
+
+    if (needsAuth === true) {
+        const token = userService.getAuthToken();
+        if (token != null) {
+            options.headers['X-Authorization'] = token;
+        }
+    }
+
+    if (body) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(body);
+    }
+
+    return options;
+}
+
+export async function makeGetRequest(url, needsAuth = false) {
+    return await request(url, getOptions('get', undefined, needsAuth))
+}
+
+export async function makePostRequest(url, data, needsAuth = false) {
+    console.log(url);
+    console.log(data);
+    return await request(url, getOptions('post', data, needsAuth));
+}
+
+export async function makePutRequest(url, data, needsAuth = false) {
+    return await request(url, getOptions('put', data, needsAuth));
+}
+
+export async function makeDeleteRequest(url, needsAuth = false) {
+    return await request(url, getOptions('delete', undefined, needsAuth));
+}
---
+++ src/services/userService.js
@@ -0,0 +1,57 @@
+import { makeGetRequest, makePostRequest } from "../helpers/serverCommunication.js";
+import { loginUserUrl, logoutUserUrl, registerUserUrl } from "../constants.js";
+
+export async function register(firstName, lastName, email, password, repeatPassword) {
+    const result = await makePostRequest(registerUserUrl, { firstName, lastName, email, password, repeatPassword });
+
+    if (result.hasOwnProperty('accessToken')) {
+        sessionStorage.setItem('userId', result._id);
+        sessionStorage.setItem('email', result.email);
+        sessionStorage.setItem('authToken', result.accessToken);
+        sessionStorage.setItem('userInfo', JSON.stringify(result));
+    }
+
+    return result;
+};
+
+export async function login(email, password) {
+    const result = await makePostRequest(loginUserUrl, { email, password });
+
+    if (result.hasOwnProperty('accessToken')) {
+        sessionStorage.setItem('userId', result._id);
+        sessionStorage.setItem('email', result.email);
+        sessionStorage.setItem('authToken', result.accessToken);
+        sessionStorage.setItem('userInfo', JSON.stringify(result));
+    }
+
+    return result;
+};
+
+export async function logout() {
+    const result = await makeGetRequest(logoutUserUrl, true);
+
+    if (result.ok === true) {
+        sessionStorage.removeItem('userId');
+        sessionStorage.removeItem('email');
+        sessionStorage.removeItem('authToken');
+        sessionStorage.removeItem('userInfo');
+    }
+
+    return result;
+};
+
+export function getCurrentUserId() {
+    return sessionStorage.getItem('userId');
+};
+
+export function getCurrentUserEmail() {
+    return sessionStorage.getItem('email');
+};
+
+export function getAuthToken() {
+    return sessionStorage.getItem('authToken');
+}
+
+export function getCurrentUserInfo() {
+    return JSON.parse(sessionStorage.getItem('userInfo'));
+}
