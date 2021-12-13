24da8a5fb4986b6b9e4350c87eddfb157d0ac6f1
reeidi <reeidi@abv.bg>
2021-12-13 02:18:27 +0200

    Fix DrawingDetails

--- src/components/DrawingDetails/DrawingDetails.js
+++ src/components/DrawingDetails/DrawingDetails.js
@@ -6,23 +6,24 @@ export default function DrawingDetails({
     info
 }) {
     return (
-        <div className="block-1 top-5">
-            <div className="block-1-shadow">
-                <h2 className="clr-6 p6">{`${author.name}'s drawing`}</h2>
+        <div className={styles.container}>
+            <div className={styles.containerShadow}>
+                <h2 className={styles.title}>{`${author.name}'s drawing`}</h2>
 
-                <div className="pad-3">
-                    <img src={imageUrl} alt="" className="img-border img-indent" />
+                <div className="pad-2">
+                    <img src={imageUrl} alt="" className={styles.imageBorder} />
                     <div className="extra-wrap clr-6">
                         <p><strong>{author.name}, {author.age} years old</strong></p>
                         <p>{info}</p>
                     </div>
 
-                    <div className="btns pad-2">
-                        <a href="#" className="link-2">Edit</a>
-                        <a href="#" className="link-2">Delete</a>
+                    <div className="pad-2">
+                        <input type="submit" className={styles.button} value="Edit" />
+                        <input type="submit" className={styles.button} value="Delete" />
                     </div>
                 </div>
             </div>
         </div>
     );
 };
+
