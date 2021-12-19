a6c275f4f3ca8748109e630ed2b0ebe2f7cdd682
reeidi <reeidi@abv.bg>
2021-12-20 01:47:00 +0200

    Home page

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -1,48 +1,44 @@
+import { Link } from "react-router-dom";
+import styles from "./HomePage.module.css";
+
 export default function HomePage() {
     return (
         <section id="content">
-            <div class="container_12">
-                <div class="grid_4 bot-1">
-                    <div class="art"></div>
-                    <h2 class="top-1 p2">Events</h2>
-                    <p class="text-1 p3">April 18 - “Spring’s Bloom”</p>
-                    <p>The PSD source files of this Art School Template are available for free for the registered members of
-                        TemplateMonster.com. Feel free to <br />
-                        get them!</p>
-                    <p class="text-1 top-2 p3">April 01 - “Smile!”</p>
-                    <p>This website template has several pages: Home Page, About Us, Schedule, Gallery, Contact Us (note that
-                        contact us form – <br />
-                        doesn’t work).</p>
-                    <a href="#" class="link-1 top-3">News Archive</a>
+            <div className="container_12">
+                <div className="grid_4 bot-1">
+                    <div className="art"></div>
+                    <h2 className={`top-1 ${styles.sectionTitle}`}>Events</h2>
+                    <p className="text-1 p3">April 18 - "Spring's Bloom"</p>
+                    <p className={styles.sideText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                    <p className="text-1 top-2 p3">April 01 - “Smile!”</p>
+                    <p className={styles.sideText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                    <Link to="/schedule" className={`${styles.link} ${styles.orange}`}>More events</Link>
                 </div>
-                <div class="grid_8">
-                    <div class="pad-1">
-                        <h2 class="p2">Welcome to Art School</h2>
-                        <p class="text-1">Art School is one of free website templates by TemplateMonster.com team. This template is
-                            optimized for 1280X1024 screen resolution. It is also XHTML &amp; CSS valid.</p>
+                <div className="grid_8">
+                    <div className="pad-1">
+                        <h2 className="p2">Welcome to Art School</h2>
+                        <p className="text-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
                     </div>
-                    <div class="block-1">
-                        <div class="block-1-shadow">
-                            <h2 class="clr-6 p4">Our Gallery</h2>
-                            <div class="box-1"> <a href="#" class="img-border"><img src="images/img1.jpg" alt="" /></a>
-                                <p class="text-2">Jennifer, 10 years</p>
-                            </div>
-                            <div class="box-1 last"> <a href="#" class="img-border"><img src="images/img2.jpg" alt="" /></a>
-                                <p class="text-2">Martin, 13 years</p>
-                            </div>
-                            <div class="clear p5"></div>
-                            <div class="box-1"> <a href="#" class="img-border"><img src="images/img3.jpg" alt="" /></a>
-                                <p class="text-2">Sebastian, 14 years</p>
-                            </div>
-                            <div class="box-1 last"> <a href="#" class="img-border"><img src="images/img4.jpg" alt="" /></a>
-                                <p class="text-2">Fiona, 8 years</p>
-                            </div>
-                            <div class="clear"></div>
-                            <div class="pad-2"> <a href="#" class="link-2">Full Gallery</a> </div>
+                    <div className="block-1">
+                        <div className="block-1-shadow">
+                            <h2 className={styles.sectionTitle}>News</h2>
+                            <p class="clr-6">
+                                <strong>Today...</strong>
+                            </p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+
+                            <div className="clear"></div>
+                            <div className="pad-2"> <Link to="/about" className={`${styles.link} ${styles.green}`}>About</Link> </div>
                         </div>
                     </div>
                 </div>
-                <div class="clear"></div>
+                <div className="clear"></div>
             </div>
         </section>
     );
