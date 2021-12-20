d725b25dd986f729811d55a61ab8fe31970a4a42
reeidi <reeidi@abv.bg>
2021-12-20 02:24:50 +0200

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer>
-            <p>© 2012 Art School</p>
+        <footer style={{color: "bisque"}}>
+            <p>© 2021 Art School</p>
             <p>Website Template by <a target="_blank" href="http://www.templatemonster.com/" class="link">TemplateMonster.com</a></p>
         </footer>
     );
